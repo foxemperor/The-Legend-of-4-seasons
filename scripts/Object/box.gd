aab4085fadfc56277b7bca05a250d154708dc9f2
KasterPK <aleksandr170702@inbox.ru>
2024-06-21 15:56:29 +0300

    Reapply "Update chomb_b.tscn"
    This reverts commit 6ab274b8af4c60d9e163806a89439436e1a90ca6.

--- scripts/Object/box.gd
+++ scripts/Object/box.gd
@@ -1,25 +1,25 @@
 extends CharacterBody2D
 
-@export var move_speed := 20
-@export var contact_duration = 1.0 # Время контакта для перемещения
-var player_contact_timer = 0.0
-var is_player_overlapping = false
+# Скорость движения коробки
+@export var move_speed = 20
+
+# Сила толчка
+@export var push_force = 50
+
+# Переменная для хранения тела игрока, который толкает коробку
+var pushing_body: CharacterBody2D = null
 
 func _physics_process(delta):
-	if is_player_overlapping:
-		player_contact_timer += delta
-		if player_contact_timer >= contact_duration:
-			var direction = get_node("/root/Player").global_position - global_position
-			direction = direction.normalized()
-			velocity = direction * move_speed
-			move_and_slide()
-	else:
-		player_contact_timer = 0.0
+	# Двигаем коробку, если есть толчок
+	if pushing_body != null:
+		var push_direction = (global_position - pushing_body.global_position).normalized()
+		velocity = push_direction * move_speed
+		move_and_slide()
 
-func _on_body_entered(body):
+func _on_area_body_entered(body):
 	if body.is_in_group("player"):
-		is_player_overlapping = true
+		pushing_body = body
 
-func _on_body_exited(body):
-	if body.is_in_group("player"):
-		is_player_overlapping = false
+func _on_area_body_exited(body):
+	if body == pushing_body:
+		pushing_body = null
